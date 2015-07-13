e988f1bb99078015cb8525e75ac57f70aab2c1aa
Liang Zhao <amztion@gmail.com>
2015-07-13 10:55:36 +0800

    add command input, command.h

---
+++ Cli/command.h
@@ -0,0 +1,12 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+int do_connect(char *ip, int *sock_fd);
+int do_get(const char *src, const char *dst, int sock_fd);
+int do_put(const char *src, const char *dst, int sock_fd);
+int do_cd(char *path);
+int do_ls(char *path);
+int do_serv_cd(char *path, int sock_fd);
+int do_serv_ls(char *path, int sock_fd);
+int bye(int sock_fd);
--- Cli/main.c
+++ Cli/main.c
@@ -1,2 +1,35 @@
 #include "common.h"
 #include "command.h"
+
+int main(int argc, const char *argv[])
+{
+    char input_buffer[100];
+    command_line command;
+    int sock_fd;
+
+
+
+    memset(input_buffer, '\0', 100);
+    while(1){
+        printf("Input command: ");
+        fgets(input_buffer, 100, stdin);
+        size_t command_len = strlen(input_buffer);
+        input_buffer[command_len - 1] = '\0';
+
+        /*
+         * Command parse
+        if(split(&command, input_buffer)){
+            switch (parse_command(command)){
+
+            }
+        }else{
+            printf("Command invalid\n");
+        }
+        */
+
+        do_connect("127.0.0.1", &sock_fd);
+
+        memset(input_buffer, '\0', 100);
+    }
+    return 0;
+}
